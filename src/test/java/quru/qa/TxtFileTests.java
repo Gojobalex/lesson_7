77d7049e5df10f431cee470d07e17577dff5b791
Alexsandr <alunev.work@gmail.com>
2021-08-04 12:40:04 +0300

    Added txt test

---
+++ src/test/java/quru/qa/TxtFileTests.java
@@ -0,0 +1,20 @@
+package quru.qa;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static utils.Files.readTextFromPath;
+
+public class TxtFileTests {
+    @Test
+    void txtTest() throws IOException {
+        String txtFilePath = "./src/test/resources/files/1.txt";
+        String expectedData = "project";
+
+        String actualData = readTextFromPath(txtFilePath);
+        assertThat(actualData, containsString(expectedData));
+    }
+}
