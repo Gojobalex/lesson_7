a259ee0074199df5181608af40baa8595561d6c9
Alexsandr <alunev.work@gmail.com>
2021-08-04 16:15:47 +0300

    Commit 3

--- src/test/java/quru/qa/TxtFileTests.java
+++ src/test/java/quru/qa/TxtFileTests.java
@@ -33,4 +33,12 @@ public class TxtFileTests {
         String actualData = readTextFromPath(txtFilePath);
         assertThat(actualData, containsString(expectedData));
     }
+    @Test
+    void txtTest4() throws IOException {
+        String txtFilePath = "./src/test/resources/files/1.txt";
+        String expectedData = "project";
+
+        String actualData = readTextFromPath(txtFilePath);
+        assertThat(actualData, containsString(expectedData));
+    }
 }
